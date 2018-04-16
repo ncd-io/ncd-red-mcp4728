dfccb8a82260060fba401837e0275d1f80be62ca
Trey Felton <trey@controleverything.com>
2018-04-16 09:42:25 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,163 @@
+module.exports = class MCP4725{
+	constructor(addr, comm, config){
+		if(typeof config != 'object') config = {};
+		this.config = Object.assign({
+			vr_1: 1,
+			pd_1: 0,
+			gx_1: 0,
+			eeprom_persist_1: false,
+			eeprom_startup_1: false,
+			eeprom_vr_1: 1,
+			eeprom_pd_1: 0,
+			eeprom_gx_1: 0,
+			eeprom_dac_1: 2048,
+
+			vr_2: 1,
+			pd_2: 0,
+			gx_2: 0,
+			eeprom_persist_2: false,
+			eeprom_startup_2: false,
+			eeprom_vr_2: 1,
+			eeprom_pd_2: 0,
+			eeprom_gx_2: 0,
+			eeprom_dac_2: 2048,
+
+			vr_3: 1,
+			pd_3: 0,
+			gx_3: 0,
+			eeprom_persist_3: false,
+			eeprom_startup_3: false,
+			eeprom_vr_3: 1,
+			eeprom_pd_3: 0,
+			eeprom_gx_3: 0,
+			eeprom_dac_3: 2048,
+
+			vr_4: 1,
+			pd_4: 0,
+			gx_4: 0,
+			eeprom_persist_4: false,
+			eeprom_startup_4: false,
+			eeprom_vr_4: 1,
+			eeprom_pd_4: 0,
+			eeprom_gx_4: 0,
+			eeprom_dac_4: 2048
+		}, config);
+		this.comm = comm;
+		this.addr = addr;
+		this.initialized = false;
+		this.status = {};
+		this.raw = [];
+		this.settable = ["channel_1", "channel_2", "channel_3", "channel_4", "all"];
+		this.init();
+	}
+	init(){
+		this.get().then((status) => {
+			var promises = [];
+			for(var i=1;i<5;i++){
+				if(!this.config["eeprom_startup_"+i]) continue;
+				var expected = this.buildCommand(
+					this.config["eeprom_vr_"+i],
+					this.config["eeprom_pd_"+i],
+					this.config["eeprom_gx_"+i],
+					this.config["eeprom_dac_"+i]);
+				if(status["channel_"+i].eeprom.raw != ((expected[0] << 8) | expected[1])){
+					promises.push(this.set(i, this.config["eeprom_dac_"+i], this.config["eeprom_vr_"+i], this.config["eeprom_pd_"+i], this.config["eeprom_gx_"+i], true));
+				}
+			}
+			if(promises.length == 0){
+				this.initialized = true;
+				return;
+			}
+			Promise.all(promises).then(() => {
+				this.initialized = true;
+			}).catch((err) => {
+				console.log(err);
+				this.initialized = false;
+			})
+		}).catch((err) => {
+			console.log(err);
+			this.initialized = false;
+		})
+	}
+	getBitsLow(v,b,l){
+		var m = ((1 << l) - 1) << (b-l);
+		return (v & m) >> (b-l);
+	}
+	parseStatus(status){
+		for(var i=0;i<4;i+=1){
+			var ch = i*6;
+			var channel = status.slice(ch, ch+6)
+			this.status["channel_"+(i+1)] = {
+				ch: this.getBitsLow(channel[0], 6, 2),
+				vr: this.getBitsLow(channel[1], 8, 1),
+				pd: this.getBitsLow(channel[1], 7, 2),
+				gx: this.getBitsLow(channel[1], 5, 1),
+				dac: (this.getBitsLow(channel[1], 4, 4) << 8) | channel[2],
+				raw: [channel[1], channel[2]],
+				eeprom: {
+					vr: this.getBitsLow(channel[4], 8, 1),
+					pd: this.getBitsLow(channel[4], 7, 2),
+					gx: this.getBitsLow(channel[4], 5, 1),
+					dac: (this.getBitsLow(channel[4], 4, 4) << 8) | channel[5],
+					raw: (channel[4] << 8) | channel[5]
+				}
+			};
+		}
+		return this.status;
+	}
+	buildCommand(vr, pd, gx, dac){
+		return [
+			(vr << 7) | (pd << 5) | (gx << 4) | (dac >> 8),
+			(dac & 255)
+		];
+	}
+	get(){
+		return new Promise((fulfill, reject) => {
+			this.comm.readBytes(this.addr, 24).then((r) => {
+				this.initialized = true;
+				fulfill(this.parseStatus(r));
+			}).catch((err) => {
+				this.initialized = false;
+				reject(err);
+			});
+		});
+	}
+	set(dac, val, nostat){
+		var _dac = this;
+		return new Promise((fulfill, reject) => {
+			var reg = 64;
+			var promises = [];
+			if(dac < 1){
+				var cmd = reg;
+				for(var i=0;i<4;i++){
+					promises.push(_dac.set(i+1, val, true));
+				}
+			}else if(dac < 5){
+				reg |= ((dac - 1) << 1);
+				if(this.config["eeprom_persist_"+dac]) reg |= 24;
+				var bytes = _dac.buildCommand(this.config["vr_"+dac], this.config["pd_"+dac], this.config["gx_"+dac], val);
+				promises.push(_dac.comm.writeBytes(_dac.addr, reg, ...bytes));
+			}else{
+				reject('Invalid DAC channel: '+dac);
+				return;
+			}
+			Promise.all(promises).then(() => {
+				if(nostat){
+					fulfill();
+					return;
+				}
+				_dac.get().then((status) => {
+					_dac.initialized = true;
+					fulfill(status);
+				}).catch((err) => {
+					this.initialized = false;
+					reject(err);
+				});
+			}).catch((err) => {
+				this.initialized = false;
+				reject(err);
+			});
+
+		});
+	}
+}
---
+++ mcp4728.js
@@ -0,0 +1,118 @@
+"use strict";
+
+const MCP4728 = require("./index.js");
+const comm = require("ncd-red-comm");
+
+process.on('unhandledRejection', error => {
+  console.log('unhandledRejection', error);
+});
+
+
+module.exports = function(RED){
+	var sensor_pool = {};
+	var loaded = [];
+
+	//ensureDependencies(['node-red-contrib-aws', 'fail']);
+
+	function NcdI2cDeviceNode(config){
+		RED.nodes.createNode(this, config);
+		this.interval = parseInt(config.interval);
+		this.addr = parseInt(config.addr);
+		if(typeof sensor_pool[this.id] != 'undefined'){
+			//Redeployment
+			clearTimeout(sensor_pool[this.id].timeout);
+			delete(sensor_pool[this.id]);
+		}
+
+		this.sensor = new MCP4728(this.addr, RED.nodes.getNode(config.connection).i2c, config);
+		sensor_pool[this.id] = {
+			sensor: this.sensor,
+			timeout: 0,
+			node: this
+		}
+		if(config.persist){
+			this.settings = comm.NcdSettings(config, this.context().global);
+		}
+		var node = this;
+		var last_status = false;
+
+		function device_status(){
+			if(!node.sensor.initialized){
+				node.status({fill:"red",shape:"ring",text:"disconnected"});
+				return false;
+			}
+			node.status({fill:"green",shape:"dot",text:"connected"});
+			return true;
+		}
+
+		function send_payload(_status, force){
+			if(!force && JSON.stringify(_status) == last_status) return;
+			var msg = [],
+				dev_status = {topic: 'device_status', payload: {
+					channel_1: _status.channel_1.dac,
+					channel_2: _status.channel_2.dac,
+					channel_3: _status.channel_3.dac,
+					channel_4: _status.channel_4.dac,
+				}};
+
+			if(config.output_all){
+				var old_status = last_status ? JSON.parse(last_status) : false;
+				for(var i in _status){
+					if(!force && old_status && _status[i].dac == old_status[i].dac){
+						msg.push(null);
+					}else msg.push({topic: i, payload: _status[i].dac})
+				}
+				msg.push(dev_status);
+			}else{
+				msg = dev_status;
+			}
+			last_status = JSON.stringify(_status);
+			node.send(msg);
+		}
+
+		function get_status(force){
+			clearTimeout(sensor_pool[node.id].timeout);
+			if(device_status(node)){
+				node.sensor.get().then((res) => {
+					send_payload(res, force);
+				}).catch((err) => {
+					node.send({error: err});
+				});
+			}else{
+				clearTimeout(sensor_pool[node.id].timeout);
+				node.sensor.init();
+				sensor_pool[node.id].timeout = setTimeout(() => {
+					if(typeof sensor_pool[node.id] != 'undefined'){
+						get_status(force);
+					}
+				}, 3000);
+			}
+		}
+
+		node.on('input', (msg) => {
+			clearTimeout(sensor_pool[node.id].timeout);
+			if(msg.topic != 'get_status'){
+				if(typeof node.sensor.settable != 'undefined' && node.sensor.settable.indexOf(msg.topic) > -1){
+					var topic = msg.topic == "all" ? 0 : msg.topic.substr(-1)*1;
+					node.sensor.set(topic, msg.payload).then().catch((err) => {
+						console.log(err);
+					}).then(() => {
+						get_status(true)
+					});
+				}
+			}else{
+				get_status(true);
+			}
+		});
+
+		node.on('close', (removed, done) => {
+			if(removed){
+				delete(sensor_pool[node.id]);
+			}
+			done();
+		});
+
+		get_status(true);
+	}
+	RED.nodes.registerType("ncd-mcp4728", NcdI2cDeviceNode)
+}
